ff7524235b1b8626f1418a6369a7f2a91dfb8931
edisonyoung1987 <edison_young@aliyun.com>
2019-04-24 16:23:07 +0800

    增加mybatis-config以及测试类

---
+++ src/test/java/V2_mybatis_TEST.java
@@ -0,0 +1,31 @@
+import com.edison.dao.BlogMapper;
+import com.edison.entity.Blog;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class V2_mybatis_TEST {
+    public static void main(String[] args) {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            BlogMapper mapper = session.getMapper(BlogMapper.class);
+            Blog blog = mapper.selectByPrimaryKey(101);
+        } finally {
+            session.close();
+        }
+    }
+}
