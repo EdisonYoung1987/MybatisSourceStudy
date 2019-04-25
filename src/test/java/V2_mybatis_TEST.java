c40a47d0ad0064c31d9ceab9f0253b39484292d8
edisonyoung1987 <edison_young@aliyun.com>
2019-04-25 14:12:13 +0800

    增加mybatis的扩展mapper，自定义sql语句

--- src/test/java/V2_mybatis_TEST.java
+++ src/test/java/V2_mybatis_TEST.java
@@ -1,4 +1,5 @@
 import com.edison.dao.BlogMapper;
+import com.edison.dao.extend.BlogExtMapper;
 import com.edison.entity.Blog;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -7,6 +8,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class V2_mybatis_TEST {
     public static void main(String[] args) {
@@ -22,12 +24,20 @@ public class V2_mybatis_TEST {
 
         SqlSession session = sqlSessionFactory.openSession();
         try {
+            //使用mybatis已有的sql
             BlogMapper mapper = session.getMapper(BlogMapper.class);
             Blog blog = mapper.selectByPrimaryKey(1);
             if (blog == null) {
                 System.out.println("没有该用户");
             } else {
                 System.out.println(blog.getBid() + "-" + blog.getAuthorId() + "-" + blog.getName());
+            }
+
+            //使用扩展ext的sql-自定义
+            BlogExtMapper extMapper=session.getMapper(BlogExtMapper.class);
+            List<Blog> blogs=extMapper.selectByAuthorId(1001);
+            for(Blog blog1:blogs){
+                System.out.println(blog1.getBid() + "-" + blog1.getAuthorId() + "-" + blog1.getName());
             }
         }finally {
             session.close();
